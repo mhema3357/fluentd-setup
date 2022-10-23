c7d6c5039e0246baa0e886d7003368620369cc44
Hema L <113641650+mhema3357@users.noreply.github.com>
2022-10-22 21:16:29 -0700

    Update docker-install.sh

--- bin/docker-install.sh
+++ bin/docker-install.sh
@@ -11,8 +11,8 @@ if [ "$os_version" = "Ubuntu" ]; then
 		echo "Docker is not installed on this instance so installing the docker pkg's"
 		apt-get update -y
 		apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /usr/share/keyrings/docker-archive-keyring.gpg add
-		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key --keyring /usr/share/keyrings/docker-archive-keyring.gpg add
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 		apt-get update -y
 		apt-get install docker-ce docker-ce-cli containerd.io -y
 
